faadf9b8e34f10d3f98e43ae2f1a6b92fd6f391c
Nobuyuki Aoki <support@glytoucan.org>
2016-04-28 12:42:06 +0900

    load script

---
+++ scripts/load.sql
@@ -0,0 +1,10 @@
+echo "turn off log";
+log_enable(2,1);
+# for some reason $I works but anything after first -i arg cant be referenced.
+# http://docs.openlinksw.com/virtuoso/isql.html
+echo DB.DBA.TTLP_MT (file_to_string_output('$ARGV[$I]'), '', '$ARGV[7]');
+DB.DBA.TTLP_MT (file_to_string_output('$ARGV[$I]'), '', '$ARGV[7]');
+echo checkpoint;
+checkpoint;
+echo commit WORK;
+commit WORK;
