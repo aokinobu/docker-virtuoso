3373005abcfcc42b3d66ddc77abd7d55ee65da46
aoki <aokinobu@gmail.com>
2018-04-18 00:07:58 +0900

    check for ini file

--- run.sh
+++ run.sh
@@ -1,7 +1,16 @@
 #!/bin/bash
 
-#[ -f /.setup_trac.sh ] && /bin/bash /.setup_trac.sh
-
+echo "this is the /virtuoso directory:"
 ls -al /virtuoso/
+
+if [ ! -f /virtuoso/db/virtuoso.ini ]; then
+    cp -R ./db /virtuoso/
+fi
+
+# in case of permissions issue
+if [ ! -f /virtuoso/db/virtuoso.ini ]; then
+    echo "no virtuoso configuration found, virtuoso cannot start"
+fi
+
 echo "running:>/usr/local/virtuoso-opensource/bin/virtuoso-t -df +configfile /virtuoso/db/virtuoso.ini"
 /usr/local/virtuoso-opensource/bin/virtuoso-t -df +configfile /virtuoso/db/virtuoso.ini
